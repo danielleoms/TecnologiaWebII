1bd1857628ce615437bea0f6ef92f655b9ed580d
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-03 14:20:05 -0300

    Delete Tarefa 1- Danielle Moreno directory

--- Tarefa 1- Danielle Moreno/Tarefa1.js
+++
@@ -1,8 +0,0 @@
-/*Faça um algoritmo que leia a idade de um usuário em anos e calcule quantas horas ele viveu aproximadamente.*/
-
-var idade = 5
-var horas = 8760
-
-HorasVividas = idade * horas;
-
-console.log("Ele viveu aproximadamente",HorasVividas,"horas")
