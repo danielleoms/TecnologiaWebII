440479f3b261ba5a2b081d5070c1a6cbd489040f
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-03 14:29:45 -0300

    Add files via upload

---
+++ Array/testeArray.js
@@ -0,0 +1,13 @@
+var frutas = ["manga", "banana", "laranja", "uva"]
+console.log("teste de array")
+console.log(frutas.length)
+
+frutas[4] = "Jambo"
+
+console.log(frutas.length)
+
+console.log(frutas)
+
+frutas[2] = "Limão"
+
+console.log(frutas)
---
+++ Tarefa 1/Tarefa1.js
@@ -0,0 +1,8 @@
+/*Faça um algoritmo que leia a idade de um usuário em anos e calcule quantas horas ele viveu aproximadamente.*/
+
+var idade = 5
+var horas = 8760
+
+HorasVividas = idade * horas;
+
+console.log("Ele viveu aproximadamente",HorasVividas,"horas")
