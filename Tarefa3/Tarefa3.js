24ad5c9e9ee6498678ddc206fd25fbfb758e9d16
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-03 14:30:24 -0300

    Update Tarefa3.js

--- Tarefa3/Tarefa3.js
+++ Tarefa3/Tarefa3.js
@@ -25,59 +25,40 @@ while(valido){
     }
 }
 
-    while(verdadeiro){
+while (verdadeiro) {
     complemento = prompt("Quer algum complemento para sua bebida? As opções são leite, açúcar e chantilly.")
-
-    switch(complemento){
+    switch (complemento) {
         case "leite":
             alert("Você selecionou leite.");
             custo += 0.5;
             verdadeiro = false;
-            
-            
+            break;
         case "açúcar":
             alert("Você selecionou açúcar.");
             custo += 0.25;
             verdadeiro = false;
             break;
-            
         case "chantilly":
             alert("Você selecionou chantilly.");
             custo += 1.0;
             verdadeiro = false;
             break;
-            
         default:
             alert("Complemento não selecionado.");
+            continue; // vai para o próximo loop de repetição do while sem executar o restante do código
+    }
+    while (verdadeiro) {
+        resposta = prompt("Você gostaria de adicionar outro complemento? (sim ou não)");
+        if (resposta === "sim") {
+            verdadeiro = true;
+            break; // sai do loop atual e volta para o loop anterior
+        } else if (resposta === "não") {
+            verdadeiro = false;
+            break; // sai do loop atual e do loop anterior
+        } else {
+            alert("Resposta inválida. Digite 'sim' ou 'não'.");
+        }
     }
 }
 
 alert("O custo da sua bebida é: R$" + custo)
-
-/*
-while(verdadeiro){
-    complemento = prompt("Quer algum complemento para sua bebida? As opções são leite, açúcar e chantilly.")
-    switch(complemento){
-        case "leite":
-            alert("Você selecionou leite.");
-            custo += 0.5;
-            verdadeiro = false;
-            break;
-            
-        case "açúcar":
-            alert("Você selecionou açúcar.");
-            custo += 0.25;
-            verdadeiro = false;
-            break;
-            
-        case "chantilly":
-            alert("Você selecionou chantilly.");
-            custo += 1.0;
-            verdadeiro = false;
-            break;
-            
-        default:
-            alert("Complemento não selecionado.");
-    }
-}
-*/
